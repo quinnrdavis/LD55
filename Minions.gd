4ada9e07d9f55c622dff7490eeea85f599cbb1d4
Quinn R Davis <quinn.r.davis@gmail.com>
2024-04-14 12:04:43 -0700

    Added ape minion

--- Minions.gd
+++ Minions.gd
@@ -4,10 +4,13 @@ extends Node2D
 
 var health
 var imp_ready = true
+var ape_ready = true
 
 @onready var spawnPoint = $SpawnPoint
 @onready var imp_timer = $ImpTimer
 @onready var imp = preload("res://imp.tscn")
+@onready var ape_timer = $ApeTimer
+@onready var ape = preload("res://ape.tscn")
 @onready var ui = $"../UI"
 @onready var tower_health_bar = $"../Minions/Tower/TowerHealthBar"
 
@@ -22,12 +25,21 @@ func _process(delta):
 	pass
 
 func spawn_minion(minion):
-	if imp_ready == true:
-		imp_ready = false
-		imp_timer.start()
-		var impInstance = imp.instantiate()
-		add_child(impInstance)
-		impInstance.global_position = spawnPoint.global_position
+	match minion:
+		"imp":
+			if imp_ready == true:
+				imp_ready = false
+				imp_timer.start()
+				var impInstance = imp.instantiate()
+				add_child(impInstance)
+				impInstance.global_position = spawnPoint.global_position
+		"ape":
+			if ape_ready == true:
+				ape_ready = false
+				ape_timer.start()
+				var apeInstance = ape.instantiate()
+				add_child(apeInstance)
+				apeInstance.global_position = spawnPoint.global_position
 
 func take_damage(damage):
 	health -= damage
@@ -35,3 +47,7 @@ func take_damage(damage):
 
 func _on_imp_timer_timeout():
 	imp_ready = true
+
+
+func _on_ape_timer_timeout():
+	ape_ready = true
--- UI.gd
+++ UI.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var minions = $"../Minions"
 @onready var imp_button = $BoxContainer/ImpContainer/ImpButton
+@onready var ape_button = $BoxContainer/VBoxContainer/ApeButton
 
 
 signal spawn_minion(minion)
@@ -11,6 +12,20 @@ func _process(delta):
 		imp_button.disabled = true
 	else:
 		imp_button.disabled = false
+	if minions.ape_ready == false:
+		ape_button.disabled = true
+	else:
+		ape_button.disabled = false
 
-func _on_texture_button_pressed(button):
+
+
+
+func _on_button_pressed():
+	get_tree().quit()
+
+
+func _on_ape_button_pressed(button):
+	spawn_minion.emit(button)
+
+func _on_imp_button_pressed(button):
 	spawn_minion.emit(button)
